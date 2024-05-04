5e852534347f102bf1b4df8e45cc506f93e7007f
Amber Kansfield <amber.kansfield@gmail.com>
2024-05-04 09:52:47 -0500

    Completed GLAB 303.5.2

---
+++ java_classwork/JavaClasswork/src/main/java/glab_303_5_2/SentinelValueDoWhileTest.java
@@ -0,0 +1,20 @@
+package glab_303_5_2;
+
+import java.util.Scanner;
+
+public class SentinelValueDoWhileTest {
+    // Checking to see if I could edit the Sentinel Value Demo to be more readable using do while
+    public static void main(String[] args) {
+        // Create a scanner
+        Scanner input = new Scanner(System.in);
+
+        int sum = 0;
+        int data;
+        do {
+            System.out.print("Enter an int value (the program exits if the input is 0: ");
+            data = input.nextInt();
+            sum += data;
+        } while (data != 0);
+        System.out.println("The sum is " + sum);
+    }
+}
